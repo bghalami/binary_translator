943c4386b1d9f403d08c3456aa7c0dfdf2e9e0b0
bghalami <benjaminghalami@gmail.com>
2018-07-05 13:58:35 -0600

    finished binary to text translator

--- lib/binary_translator.rb
+++ lib/binary_translator.rb
@@ -30,14 +30,52 @@ class BinaryTranslator
       "y" => "011001",
       "z" => "011010"
     }
+
+    @binary_to_alpha = {
+    "000000" => " ",
+    "000001" => "a",
+    "000010" => "b",
+    "000011" => "c",
+    "000100" => "d",
+    "000101" => "e",
+    "000110" => "f",
+    "000111" => "g",
+    "001000" => "h",
+    "001001" => "i",
+    "001010" => "j",
+    "001011" => "k",
+    "001100" => "l",
+    "001101" => "m",
+    "001110" => "n",
+    "001111" => "o",
+    "010000" => "p",
+    "010001" => "q",
+    "010010" => "r",
+    "010011" => "s",
+    "010100" => "t",
+    "010101" => "u",
+    "010110" => "v",
+    "010111" => "w",
+    "011000" => "x",
+    "011001" => "y",
+    "011010" => "z"
+    }
   end
 
-  def translate(string_to_translate)
-    array_to_translate = string_to_translate.downcase.chars
-    binary_array = array_to_translate.map do |letter|
+  def translate(text_string_to_translate)
+    text_array_to_translate = text_string_to_translate.downcase.chars
+    binary_array = text_array_to_translate.map do |letter|
       @alpha_to_binary[letter]
     end
     binary_array.join
   end
+
+ def translate_to_text(binary_string_to_translate)
+   binary_array_to_translate = binary_string_to_translate.scan(/.{6}/)
+   text_array = binary_array_to_translate.map do |number|
+     @binary_to_alpha[number]
+   end
+   text_array.join
+ end
 
 end
