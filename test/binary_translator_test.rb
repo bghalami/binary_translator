3ddb70a035ad868bef0dfc4ef4dd0f818cd4b7f6
bghalami <benjaminghalami@gmail.com>
2018-07-05 13:30:28 -0600

    added spaces and characters test

--- test/binary_translator_test.rb
+++ test/binary_translator_test.rb
@@ -11,4 +11,17 @@ class BinaryTranslatorTest < Minitest::Test
     assert_equal "010100010101010010001001001110000111", bt.translate("TURING")
   end
 
+  def test_that_spaces_return_00000
+    bt = BinaryTranslator.new
+
+    assert_equal "001000000101001100001100001111000000010111001111010010001100000100", bt.translate("Hello World!")
+    assert_equal "00000", bt.translate(" ")
+  end
+
+  def test_characters_return_nothing
+    bt = BinaryTranslator.new
+
+    assert_equal "", bt.translate("!@{$#%^&*()}")
+  end
+
 end
